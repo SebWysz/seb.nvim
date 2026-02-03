04882b2ce5e6f84391d298b052fe321fe7e1178c
SebWysz <sebwyszynski@yahoo.com>
2026-02-03 13:04:12 -0600

    Small change

--- lua/custom/plugins/nvim-lspconfig.lua
+++ lua/custom/plugins/nvim-lspconfig.lua
@@ -153,9 +153,7 @@ return {
     local servers = {
       clangd = {},
       -- gopls = {},
-      pyright = {
-        cmd = { vim.fn.expand '~/memsql/psyduck/runner/docker/default/.venv/bin/pyright-langserver', '--stdio' },
-      },
+      pyright = {},
       rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
